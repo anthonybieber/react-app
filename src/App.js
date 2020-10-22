01843b88928b45f9767946538c423dd585729e18
abieber <anthonybieber@Anthonys-MacBook-Pro.local>
2020-10-22 11:43:45 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn From Anthony Now
+          Learn From Anthony Now YAY
         </a>
       </header>
     </div>
